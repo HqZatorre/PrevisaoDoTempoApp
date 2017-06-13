2b9ed3481087b6f5ff3d087131600065d7380432
HqZatorre <henriquezatorre@gmail.com>
2017-06-13 19:08:24 -0300

    DataNoDisplay

--- app/src/main/java/com/exercicio/hqzatorre/previsaodotempo/MainActivity.java
+++ app/src/main/java/com/exercicio/hqzatorre/previsaodotempo/MainActivity.java
@@ -2,6 +2,11 @@ package com.exercicio.hqzatorre.previsaodotempo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +14,11 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Calendar data = Calendar.getInstance();
+        TextView dataView = (TextView) findViewById(R.id.data);
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy H:mm");
+        dataView.setText(format.format(data.getTime()));
+
     }
 }
